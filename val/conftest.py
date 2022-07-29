d271f4b10cdf4c6d2795e7724ec7a7ad014c7789
Tarek Aloui <taloui@college.harvard.edu>
2022-07-29 13:32:56 -0400

    adding fixture 1

--- val/conftest.py
+++ val/conftest.py
@@ -1,13 +1,15 @@
 import pytest
 
-def pytest_addoption(parser):
-        parser.addoption(
-            "--num-sys-name",
-            action="store",
-            default="fp32",
-            help="provide a num sys arg to run the test with",
-        )
 
-    @pytest.fixture
-    def cmdopt(request):
-        return request.config.getoption("--num-sys-name")
+def pytest_addoption(parser):
+    parser.addoption(
+        "--num-sys-name",
+        action="store",
+        default="fp32",
+        help="provide a num sys arg to run the test with",
+    )
+
+
+@pytest.fixture
+def params(request):
+    return request.config.getoption("--num-sys-name")
