5012406c96f3ff89b61ce5044162aea363680b67
Tarek Aloui <taloui@college.harvard.edu>
2022-08-19 10:45:49 -0400

    switch match to if statement

--- src/num_sys_class.py
+++ src/num_sys_class.py
@@ -12,7 +12,7 @@ num_sys = load(
     sources=[
         os.path.join(current_path, "num_sys.cpp"),
         os.path.join(current_path, "num_sys_helper.cpp"),
-    ]
+    ],
 )
 
 
@@ -20,6 +20,7 @@ class _number_sys:
     """
     General class for number systems, used to bit_flip using a specific format
     """
+
     def bit_flip(self, bit_arr, bit_ind):
         # interpret index from least significant bit
         bit_ind_LSB = len(bit_arr) - 1 - bit_ind
@@ -109,14 +110,9 @@ class _number_sys:
 
 class _ieee754(_number_sys):
     """IEEE Standard 754 Floating Point Number System"""
+
     def __init__(
-            self,
-            exp_len=8,
-            mant_len=23,
-            bias=None,
-            denorm=True,
-            max_val=None,
-            min_val=None
+        self, exp_len=8, mant_len=23, bias=None, denorm=True, max_val=None, min_val=None
     ):
         self.exp_len = exp_len
         self.mant_len = mant_len
@@ -151,13 +147,13 @@ class _ieee754(_number_sys):
         if int_str.find("1") != -1:
             # decimal shift
             ind = len(int_str) - 1 - int_str.index("1")
-            int_str = int_str[len(int_str) - ind - 1:]
+            int_str = int_str[len(int_str) - ind - 1 :]
             exp_str = _number_sys.int_to_bin(ind + self.bias)
         else:
             if frac_str.find("1") != -1:
                 dec_shift = frac_str.index("1") + 1
                 if dec_shift > self.bias:
-                    frac_str = frac_str[self.bias:]
+                    frac_str = frac_str[self.bias :]
                 else:
                     exp_str = _number_sys.int_to_bin(-dec_shift + self.bias)
                     frac_str = frac_str[dec_shift:]
@@ -168,10 +164,14 @@ class _ieee754(_number_sys):
         mant_str = (mant_str + ("0" * (self.mant_len - len(mant_str))))[: self.mant_len]
 
         # asserts
-        assert len(exp_str) == self.exp_len,\
-            "exp_len unknown error: %d != %d" %(len(exp_str), self.exp_len)
-        assert len(mant_str) == self.mant_len,\
-            "mant_len unknown error: %d != %d" %(len(mant_str), self.mant_str)
+        assert len(exp_str) == self.exp_len, "exp_len unknown error: %d != %d" % (
+            len(exp_str),
+            self.exp_len,
+        )
+        assert len(mant_str) == self.mant_len, "mant_len unknown error: %d != %d" % (
+            len(mant_str),
+            self.mant_str,
+        )
 
         return list("".join([sign, exp_str, mant_str]))
 
@@ -186,19 +186,19 @@ class _ieee754(_number_sys):
 
         sign = pow(-1, int(bit_arr[0]))
 
-        exp_str = "".join(bit_arr[1:self.exp_len + 1])
+        exp_str = "".join(bit_arr[1 : self.exp_len + 1])
         exp = int(exp_str, 2) - self.bias
 
-        mant_str = "".join(bit_arr[self.exp_len + 1:])
+        mant_str = "".join(bit_arr[self.exp_len + 1 :])
         mant = mant_to_int(mant_str)
 
         # exceptions
         if exp_str == "0" * self.exp_len and mant_str == "0" * self.mant_len:
             return 0
         if exp_str == "1" * self.exp_len and mant_str == "0" * self.mant_len:
-            return sign * float('inf')
+            return sign * float("inf")
         if exp_str == "1" * self.exp_len and mant_str != "0" * self.mant_len:
-            return float('nan')
+            return float("nan")
 
         # handling denormals
         if exp_str == "0" * self.exp_len and mant_str != "0" * self.mant_len:
@@ -224,13 +224,14 @@ class _ieee754(_number_sys):
         return list(int_str)
 
     def bitstream_to_int(self, bit_arr):
-        exp_str = "".join(bit_arr[1: self.exp_len + 1])
+        exp_str = "".join(bit_arr[1 : self.exp_len + 1])
         exp = int(exp_str, 2)
         return exp
 
 
 class num_fp32(_ieee754):
     """Floating Point 32 Number System"""
+
     def __init__(self):
         super(num_fp32, self).__init__()
 
@@ -240,6 +241,7 @@ class num_fp32(_ieee754):
 
 class num_fp16(_ieee754):
     """Floating Point 16 Number System"""
+
     def __init__(self):
         super(num_fp16, self).__init__(exp_len=5, mant_len=10)
 
@@ -249,6 +251,7 @@ class num_fp16(_ieee754):
 
 class num_float_n(_ieee754):
     """Floating Point Number System"""
+
     # 1 bit for sign + len(integer part) + len(frac part)
     def __init__(self, exp_len=5, mant_len=10):
         super(num_float_n, self).__init__(exp_len=exp_len, mant_len=mant_len)
@@ -259,6 +262,7 @@ class num_float_n(_ieee754):
 
 class num_bfloat16(_ieee754):
     """Brain Float Number System"""
+
     def __init__(self):
         super(num_bfloat16, self).__init__(exp_len=8, mant_len=7)
 
@@ -268,6 +272,7 @@ class num_bfloat16(_ieee754):
 
 class num_fixed_pt(_number_sys):
     """Fixed Point Number System"""
+
     # 1 bit for sign + len(integer part) + len(frac part)
     def __init__(self, int_len=3, frac_len=3):
         self.int_len = int_len
@@ -281,7 +286,7 @@ class num_fixed_pt(_number_sys):
         if len(int_str) > self.int_len:
             int_str = "1" * self.int_len
 
-        frac_str = _number_sys.frac_to_bin(num - int(num))[:self.frac_len]
+        frac_str = _number_sys.frac_to_bin(num - int(num))[: self.frac_len]
 
         # zero padding
         int_str = ("0" * (self.int_len - len(int_str))) + int_str
@@ -290,34 +295,36 @@ class num_fixed_pt(_number_sys):
         return list(sign) + list(int_str) + list(frac_str)
 
     def real_to_format_tensor(self, tensor):
-        return fixed_point_quantize(tensor,
-                                    1 + self.int_len + self.frac_len,
-                                    self.frac_len)
+        return fixed_point_quantize(
+            tensor, 1 + self.int_len + self.frac_len, self.frac_len
+        )
 
     def format_to_real(self, bit_arr):
-        int_str, frac_str = map(lambda arr: "".join(arr),
-                                (bit_arr[1:self.int_len + 1],
-                                 bit_arr[self.int_len + 1:]),)
+        int_str, frac_str = map(
+            lambda arr: "".join(arr),
+            (bit_arr[1 : self.int_len + 1], bit_arr[self.int_len + 1 :]),
+        )
         sign = 1 if bit_arr[0] == "0" else -1
         return sign * (int(int_str, 2) + _number_sys.bin_to_frac(frac_str))
 
 
 class block_fp(_ieee754):
     """Block Float Number System"""
+
     # 1 bit for sign + len(integer part) + len(frac part)
     def __init__(self, bit_width=32, exp_len=8, mant_len=23):
         super(block_fp, self).__init__(exp_len=exp_len, mant_len=mant_len)
         self.bit_width = bit_width
 
     def real_to_format_tensor(self, tensor):
-        return self.quant_bfloat(float_arr=tensor,
-                                 n_bits=self.bit_width,
-                                 n_exp=self.exp_len)
+        return self.quant_bfloat(
+            float_arr=tensor, n_bits=self.bit_width, n_exp=self.exp_len
+        )
 
     def real_to_format_tensor_meta(self, tensor):
-        return self.quant_bfloat_meta(float_arr=tensor,
-                                      n_bits=self.bit_width,
-                                      n_exp=self.exp_len)
+        return self.quant_bfloat_meta(
+            float_arr=tensor, n_bits=self.bit_width, n_exp=self.exp_len
+        )
 
     def quant_bfloat_py(self, float_arr, n_bits, n_exp):
         n_mant = n_bits - 1 - n_exp
@@ -326,11 +333,11 @@ class block_fp(_ieee754):
         float_arr = torch.abs(float_arr)
 
         # 2. limits the range of output float point
-        min_exp = -2 ** (n_exp - 1) + 2
+        min_exp = -(2 ** (n_exp - 1)) + 2
         max_exp = 2 ** (n_exp - 1) - 1
 
-        min_value = 2 ** min_exp
-        max_value = (2 ** max_exp) * (2 - 2 ** (-n_mant))
+        min_value = 2**min_exp
+        max_value = (2**max_exp) * (2 - 2 ** (-n_mant))
 
         # non-denormal part
         float_arr[float_arr < min_value] = 0
@@ -354,7 +361,7 @@ class block_fp(_ieee754):
         exp_adj = torch.full(exp.shape, shared_exp, device=float_arr.device)
 
         # exp should not be larger than max_exp
-        assert (shared_exp <= max_exp)
+        assert shared_exp <= max_exp
         power_exp = torch.exp2(exp_adj)
 
         # 4. quantize mantissa
@@ -378,11 +385,11 @@ class block_fp(_ieee754):
         float_arr = torch.abs(float_arr)
 
         # 2. limits the range of output float point
-        min_exp = -2 ** (n_exp - 1) + 2
+        min_exp = -(2 ** (n_exp - 1)) + 2
         max_exp = 2 ** (n_exp - 1) - 1
 
-        min_value = 2 ** min_exp
-        max_value = (2 ** max_exp) * (2 - 2 ** (-n_mant))
+        min_value = 2**min_exp
+        max_value = (2**max_exp) * (2 - 2 ** (-n_mant))
 
         # non-denormal part
         float_arr[float_arr < min_value] = 0
@@ -419,7 +426,7 @@ class block_fp(_ieee754):
         exp_adj = torch.full(exp.shape, shared_exp, device=float_arr.device)
 
         # exp should not be larger than max_exp
-        assert (shared_exp <= max_exp)
+        assert shared_exp <= max_exp
         power_exp = torch.exp2(exp_adj)
 
         # 4. quantize mantissa
@@ -439,6 +446,7 @@ class block_fp(_ieee754):
 
 class adaptive_float(_ieee754):
     """Adaptive Float Number System"""
+
     # 1 bit for sign + len(integer part) + len(frac part)
     def __init__(self, bit_width=32, exp_len=8, mant_len=23, exp_bias=None):
         super(adaptive_float, self).__init__(exp_len=exp_len, mant_len=mant_len)
@@ -446,16 +454,20 @@ class adaptive_float(_ieee754):
         self.exp_bias = exp_bias
 
     def real_to_format_tensor(self, tensor):
-        return self.quantize_adaptivfloat(float_arr=tensor,
-                                          n_bits=self.bit_width,
-                                          n_exp=self.exp_len,
-                                          bias=self.exp_bias)
+        return self.quantize_adaptivfloat(
+            float_arr=tensor,
+            n_bits=self.bit_width,
+            n_exp=self.exp_len,
+            bias=self.exp_bias,
+        )
 
     def real_to_format_tensor_meta(self, tensor):
-        return self.quantize_adaptivfloat_meta(float_arr=tensor,
-                                               n_bits=self.bit_width,
-                                               n_exp=self.exp_len,
-                                               bias=self.exp_bias)
+        return self.quantize_adaptivfloat_meta(
+            float_arr=tensor,
+            n_bits=self.bit_width,
+            n_exp=self.exp_len,
+            bias=self.exp_bias,
+        )
 
     def quantize_adaptivfloat_py(self, float_arr, n_bits=8, n_exp=4, bias=None):
         n_mant = n_bits - 1 - n_exp
@@ -467,11 +479,11 @@ class adaptive_float(_ieee754):
         bias = (2 ** (n_exp - 1) - 1) - bias_temp
 
         # 2. limits the range of output float point
-        min_exp = -2 ** (n_exp - 1) + 2 - bias
+        min_exp = -(2 ** (n_exp - 1)) + 2 - bias
         max_exp = 2 ** (n_exp - 1) - 1 - bias
 
-        min_value = 2. ** min_exp
-        max_value = (2. ** max_exp) * (2 - 2 ** (-n_mant))
+        min_value = 2.0**min_exp
+        max_value = (2.0**max_exp) * (2 - 2 ** (-n_mant))
         # non-denormal part
         float_arr[float_arr < min_value] = 0
 
@@ -505,11 +517,7 @@ class adaptive_float(_ieee754):
         # Python
         # return self.quantize_adaptivfloat_py(float_arr, n_bits, n_exp, bias)
 
-    def quantize_adaptivfloat_meta_py(self,
-                                      float_arr,
-                                      n_bits=8,
-                                      n_exp=4,
-                                      bias=None):
+    def quantize_adaptivfloat_meta_py(self, float_arr, n_bits=8, n_exp=4, bias=None):
         n_mant = n_bits - 1 - n_exp
         # 1. store sign value and do the following part as unsigned value
         sign = torch.sign(float_arr)
@@ -531,11 +539,11 @@ class adaptive_float(_ieee754):
         # ============= ERROR INJECTION INTO META =============
 
         # 2. limits the range of output float point
-        min_exp = -2 ** (n_exp - 1) + 2 - bias
+        min_exp = -(2 ** (n_exp - 1)) + 2 - bias
         max_exp = 2 ** (n_exp - 1) - 1 - bias
 
-        min_value = 2. ** min_exp
-        max_value = (2. ** max_exp) * (2 - 2 ** (-n_mant))
+        min_value = 2.0**min_exp
+        max_value = (2.0**max_exp) * (2 - 2 ** (-n_mant))
         # non-denormal part
         float_arr[float_arr < min_value] = 0
 
@@ -559,21 +567,20 @@ class adaptive_float(_ieee754):
         float_out = sign * power_exp * mant
         return float_out
 
-    def quantize_adaptivfloat_meta(self,
-                                   float_arr,
-                                   n_bits=8,
-                                   n_exp=4,
-                                   bias=None):
+    def quantize_adaptivfloat_meta(self, float_arr, n_bits=8, n_exp=4, bias=None):
         # C++
         return num_sys.quantize_adaptivfloat_meta(float_arr, n_bits, n_exp, -1)
 
         # Python
         # return self.quantize_adaptivfloat_meta_py(float_arr, n_bits, n_exp, bias)
 
+
 #################################################################
 ################### HELPER METHODS FOR NUMSYS ###################
 #################################################################
 def string_to_numsys(name, bits=16, radix_up=5, radix_down=10, bias=None):
+    ## DESC: convert string name to numsys
+
     # common number systems in PyTorch
     if name == "fp32":
         return num_fp32(), name
@@ -603,41 +610,76 @@ def string_to_numsys(name, bits=16, radix_up=5, radix_down=10, bias=None):
     else:
         sys.exit("Config format error: Number format not supported")
 
+
 def config_to_numsys(config):
+    ## DESC: converts a configuration (eg. ("fp", 8, 23) to a number system object
+
     # check config format is correct
-    assert(type(config) is tuple)
+    assert type(config) is tuple
 
-    match config[0]:
-        case "fp32":
-            return num_fp32(), "fp32"
-        case "INT":
-            # assert getQuantize_en()
-            return num_fp32(), "INT"
-        case "fp16":
-            return num_fp16(), "fp16"
-        case "bfloat16":
-            return num_bfloat16(), "bfloat16"
-        case "fp":
-            return num_float_n(exp_len=config[1], mant_len=config[2]), "fp_n"
-        case "fxp":
-            return num_fixed_pt(int_len=config[1], frac_len=config[2]), "fxp_n"
-        case "block_fp":
-            return block_fp(bit_width=config[1], exp_len=config[2], mant_len=config[3]), "block_fp"
-        case "adaptive_fp":
-            return adaptive_float(bit_width=config[1], exp_len=config[2], mant_len=config[3], exp_bias=config[4]), "adaptive_fp"
-        case _:
-            raise Exception("Config format error: Number format not supported")
+    if config[0] is "fp32":
+        return num_fp32(), "fp32"
+    elif config[0] is "INT":
+        # assert getQuantize_en()
+        return num_fp32(), "INT"
+    elif config[0] is "fp16":
+        return num_fp16(), "fp16"
+    elif config[0] is "bfloat16":
+        return num_bfloat16(), "bfloat16"
+    elif config[0] is "fp":
+        return num_float_n(exp_len=config[1], mant_len=config[2]), "fp_n"
+    elif config[0] is "fxp":
+        return num_fixed_pt(int_len=config[1], frac_len=config[2]), "fxp_n"
+    elif config[0] is "block_fp":
+        return (
+            block_fp(bit_width=config[1], exp_len=config[2], mant_len=config[3]),
+            "block_fp",
+        )
+    elif config[0] is "adaptive_fp":
+        return (
+            adaptive_float(
+                bit_width=config[1],
+                exp_len=config[2],
+                mant_len=config[3],
+                exp_bias=config[4],
+            ),
+            "adaptive_fp",
+        )
+    else:
+        raise Exception("Config format error: Number format not supported")
 
-def to_numsys_list(lst, conversion_func):
+
+def to_numsys_list_func(lst, conversion_func):
+    ## DESC: converts a list of numsys descriptions (string name or config tuple) to a list of numsys objects
+    ######## based on a fixed conversion function
+
+    # Check list is not empty
+    assert len(lst) > 0
+
     numsys_list = []
     for config in lst:
         numsys_list.append(conversion_func(config))
     return numsys_list
 
+
+def to_numsys_list(lst):
+    ## DESC: converts a list of numsys descriptions (string name or config tuple) to a list of numsys objects
+
+    # Check list is not empty
+    assert len(lst) > 0
+
+    numsys_list = []
+    for config in lst:
+        conversion_func = string_to_numsys if type(config) is str else config_to_numsys
+        numsys_list.append(conversion_func(config))
+    return numsys_list
+
+
 def string_list_to_numsys_list(config_list):
-    to_numsys_list(config_list, string_to_numsys)
+    ## DESC: converts a list of numsys string names to a list of numsys objects
+    to_numsys_list_func(config_list, string_to_numsys)
+
 
 def config_list_to_numsys_list(config_list):
-    to_numsys_list(config_list, config_to_numsys)
-    
-
+    ## DESC: converts a list of numsys config tuples to a list of numsys objects
+    to_numsys_list_func(config_list, config_to_numsys)
